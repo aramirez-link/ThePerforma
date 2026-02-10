9c3a5e44e2f43080d16aa19be5bce6b44cf666d4
aramirez-link <andrew.ramirez@link-collective.com>
2026-02-10 00:21:11 -0500

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: process.env.SITE || "https://example.github.io/the-performa",
-  base: process.env.BASE || "/",
+  site: "https://theperforma.com",
+  base: "/",
   output: "static",
   integrations: [
     react(),
